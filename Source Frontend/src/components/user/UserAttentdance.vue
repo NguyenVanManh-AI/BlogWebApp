ff0c2d97ce3f069862776bbbb87a63f852ef8e6a
github_username <nguyenvanmanh2001it1@gmail.com>
2023-03-27 21:54:49 +0700

    Init Source code Frontend Vuejs

---
+++ Source Frontend/src/components/user/UserAttentdance.vue
@@ -0,0 +1,21 @@
+<template>
+    <div >
+        User Attentdance
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "UserAttentdance",
+    components: {
+        
+    },
+    methods: {
+    
+    }
+}
+</script>
+<style scoped>
+
+</style>
