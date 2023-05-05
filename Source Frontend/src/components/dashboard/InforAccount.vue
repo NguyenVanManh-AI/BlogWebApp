db460d307435c27073fb51fb73dd2ec49ee56752
Nguyen Van Manh <nguyenvanmanh2001it1@gmail.com>
2023-05-06 00:25:31 +0700

    Blog App Version 1.0

--- Source Frontend/src/components/dashboard/InforAccount.vue
+++ Source Frontend/src/components/dashboard/InforAccount.vue
@@ -61,10 +61,10 @@
                 <div class="modal-body">
                 <ModalPostArticle></ModalPostArticle>
                 </div>
-                <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <!-- <div class="modal-footer"> -->
+                <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
                 <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
-                </div>
+                <!-- </div> -->
             </div>
             </div>
         </div>
